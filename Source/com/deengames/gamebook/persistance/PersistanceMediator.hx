aa3da133e0a43d4e2400c226d494ff32f8b3e269
Ashiq A <ashes999@yahoo.com>
2012-09-10 20:55:14 -0400

    More temporary code. Trying to separate out validation vs. loading of content.

--- Source/com/deengames/gamebook/persistance/PersistanceMediator.hx
+++ Source/com/deengames/gamebook/persistance/PersistanceMediator.hx
@@ -11,9 +11,13 @@ class PersistanceMediator
 
 	public function new() 
 	{
-		public function loadProject(fileName:String) : Project {
-			return new PersisterV1().loadProject(fileName);
-		}
 	}
 	
+	public function loadProject(fileName:String) : Project {
+		return new PersisterV1().loadProject(fileName);
+	}
+	
+	public function validateProject(fileName:String) : String {
+		return new PersisterV1().validateProject(fileName);
+	}
 }
--- Source/com/deengames/gamebook/persistance/PersisterV1.hx
+++ Source/com/deengames/gamebook/persistance/PersisterV1.hx
@@ -1,6 +1,7 @@
 package com.deengames.gamebook.persistance;
 using com.blastcube.extensions.StringExtensions;
 import com.deengames.gamebook.model.Project;
+import nme.Assets;
 
 /**
  * ...
@@ -15,10 +16,30 @@ class PersisterV1
 		
 	}
 
-	public function loadProject(fileName:String) : Project {
-		if (!fileName.startsWith("/assets")) {
+	public function getPathRelativeToAssetsDir(fileName:String) : String
+	{
+		if (!fileName.startsWith("/assets/")) {
 			fileName = "/assets/" + fileName;
+		} else if (!fileName.startsWith("/")) {
+			fileName = "/assets/" + fileName;
+		} else {
+			fileName = "/assets" + fileName;
 		}
+		
+		return fileName;
+	}
+	
+	public function loadProject(fileName:String) : Project
+	{
+		//var fullPath = getPathRelativeToAssetsDir(fileName);
+		var content = Assets.getText(fileName);
+		if (content == "") {
+			throw "No content?!";
+		} else {
+			throw "Got it: " + content;
+		}
+		
+		return new Project("Unreachable code");
 	}
 	
 }
