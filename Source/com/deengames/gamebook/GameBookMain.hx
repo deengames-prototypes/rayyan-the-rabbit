f4eee6420b028e3a5d0fbf1b7e7f0a65ba582c9b
ashes999 <ashes999@yahoo.com>
2012-09-13 09:35:01 -0400

    Watch what you're deleting, bucko.

---
+++ Source/com/deengames/gamebook/GameBookMain.hx
@@ -0,0 +1,31 @@
+package com.deengames.gamebook;
+
+import com.blastcube.controls.ThreeScaleButtonTest;
+import com.blastcube.core.Game;
+import com.blastcube.core.Screen;
+import com.deengames.gamebook.screen.TitleScreen;
+import com.deengames.gamebook.screen.ValidateProjectScreen;
+
+/**
+ * @author ashes999
+ */
+class GameBookMain extends Game {
+	
+	private var currentScreen:Screen;
+	public static var projectFileName = "RayyanTheRabbit.xml";
+	
+	public function new () {
+		super();
+	}
+	
+	// Entry point
+	public static function main () {
+		Game.showScreen(new ValidateProjectScreen(projectFileName));
+		//Game.showScreen(new TitleScreen());
+		
+		// Run some tests instead -- use to debug tests
+		//var test = new ThreeScaleButtonTest();
+		//test.setup();
+		//test.initAddsImagesAndTextFieldToButtonAndButtonToScreen();
+	}
+}
