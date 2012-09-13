43c8cc942bda9e04cc64e2f0e513587a3d9a7d1f
Ashiq A <ashes999@yahoo.com>
2012-09-13 09:30:40 -0400

    Got vector drawing working. Allahu akbar, Allahu akbar, laa ilaaha illa Allah! - Butchered BitmapCache class. - MovieClipUtils draws vector into a bitmap.

--- Source/com/deengames/gamebook/screen/ValidateProjectScreen.hx
+++ Source/com/deengames/gamebook/screen/ValidateProjectScreen.hx
@@ -1,6 +1,6 @@
 package com.deengames.gamebook.screen;
 import com.blastcube.core.Screen;
-import com.blastcube.vectorgraphics.BitmapCache;
+import com.blastcube.vectorgraphics.MovieclipUtils;
 import com.deengames.gamebook.model.Project;
 import com.deengames.gamebook.persistance.PersistanceMediator;
 import com.deengames.gamebook.Square;
@@ -8,6 +8,7 @@ import format.SWF;
 import format.swf.MovieClip;
 import nme.Assets;
 import nme.display.Bitmap;
+import nme.display.Sprite;
 import nme.Lib;
 
 /**
@@ -26,14 +27,12 @@ class ValidateProjectScreen extends Screen
 		// Show first screen; pass entire project to the StoryScreen?
 		
 		//Lib.current.stage.addChild(new Bitmap(BitmapCache.get(Square)));
-		var swf = new SWF(Assets.getBytes("assets/square.swf"));
-		var square:MovieClip = swf.createMovieClip("Squarez");
+		var swf:SWF = new SWF(Assets.getBytes("assets/test.swf"));		
+		var square:Sprite = MovieclipUtils.getScaledBitmap(swf, "Square", 2.5);
+		//var square:MovieClip = swf.createMovieClip("Squarez");
 		Lib.current.stage.addChild(square);
 		
-		square.x = square.width;
-		square.y = square.height;
-		
-		square.scaleX *= 5;
-		square.scaleY *= 5;
+		//square.scaleX *= 2.5;
+		//square.scaleY *= 2.5;
 	}
 }
