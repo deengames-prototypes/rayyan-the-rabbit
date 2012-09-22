e23a589121d73e033a4d403277ce379119bbe2f2
Ashiq A <ashes999@yahoo.com>
2012-09-22 08:20:05 -0400

    Added titlescreen audio. Yays.

--- Source/com/deengames/gamebook/screen/TitleScreen.hx
+++ Source/com/deengames/gamebook/screen/TitleScreen.hx
@@ -4,11 +4,14 @@ import com.blastcube.core.Game;
 import com.blastcube.core.Screen;
 import com.blastcube.vectorgraphics.Resize;
 import com.deengames.gamebook.GameBookMain;
+import nme.Assets;
 import nme.display.Bitmap;
 import nme.display.Sprite;
 import nme.events.Event;
 import nme.events.MouseEvent;
 import nme.Lib;
+import nme.media.Sound;
+import nme.media.SoundChannel;
 import nme.text.TextField;
 
 /**
@@ -18,30 +21,28 @@ import nme.text.TextField;
 
 class TitleScreen extends Screen
 {	
-	
+	// Control audio playback
+	private var sound:Sound;
+	private var soundChannel:SoundChannel;
 	
 	// Can't use new; buttons need access to the current screen, which isn't set yet.
 	public function new(swfName:String="assets/swf/titlescreen.swf", symbolName:String="titlescreen") 
 	{		
 		super();
 		var background:Sprite = this.addRasterizedVector(swfName, symbolName, Resize.AtMost, this.stageWidth, this.stageHeight);
-
-		/*
-		var newGame:ThreeScaleButton = this.addButton("New Game");
-		newGame.name = "newgame";
-		newGame.setX((Lib.current.stage.stageWidth - newGame.width) / 2);
-		newGame.setY(0.75 * Lib.current.stage.stageHeight);
-
-		newGame.setClickHandler(function(event) {
-			Game.showScreen(new ShowScenesScreen(GameBookMain.projectFileName));
-		});
-		*/
 		
 		var startButton:Sprite = this.addRasterizedVector("assets/swf/buttons.swf", "startButton", Resize.AtMost, Math.floor(this.stageWidth / 5), Math.floor(this.stageWidth / 5));
 		startButton.x = (this.stageWidth - startButton.width) / 2;
 		startButton.y = this.stageHeight - (2 * startButton.height);
 		startButton.addEventListener(MouseEvent.MOUSE_DOWN, function(event:Event) {
 			Game.showScreen(new ShowScenesScreen(GameBookMain.projectFileName));
+			
+			if (this.soundChannel != null) {
+				this.soundChannel.stop();
+			}
 		});
+		
+		this.sound = Assets.getSound("assets/audio/scenes/titlescreen.mp3");
+		this.soundChannel = sound.play();
 	}
 }
