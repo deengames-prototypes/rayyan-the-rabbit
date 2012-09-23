2155cd4b77c11bcf47443084c358e3fd78e99d5a
Ashiq A <ashes999@yahoo.com>
2012-09-23 07:04:57 -0400

    - Added background audio loading as an optional XML attribute, specified in XML - Fixed a bug where clicking Next on the last page still plays audio, even while playing the "the end" audio.

--- Source/com/deengames/gamebook/model/Scene.hx
+++ Source/com/deengames/gamebook/model/Scene.hx
@@ -11,12 +11,15 @@ class Scene
 	public var name(getName, null):String;
 	public var background(getBackground, null):String;	
 	public var text(getText, null) : String;
+	public var backgroundAudio(getBackgroundAudio, null) : String;
+	public var hasBackgroundAudio(getHasBgAudio, null) : Bool;
 	
-	public function new(name:String, background:String, text:String) 
+	public function new(name:String, background:String, text:String, backgroundAudio:String = "") 
 	{
 		this.name = name;
 		this.text = text;
 		this.background = background;
+		this.backgroundAudio = backgroundAudio;
 	}
 	
 	private function getName() : String
@@ -32,5 +35,15 @@ class Scene
 	private function getText() : String
 	{
 		return this.text;
+	}
+	
+	private function getBackgroundAudio() : String
+	{
+		return this.backgroundAudio;
+	}
+	
+	private function getHasBgAudio() : Bool
+	{
+		return this.backgroundAudio != "";
 	}
 }
