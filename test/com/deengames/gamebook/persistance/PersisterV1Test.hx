265f745ab1225dd666d41f04d54db0d14dd8ca5e
admin <admin@Gateway1.phub.net.cable.rogers.com>
2012-09-27 06:20:37 -0400

    The audio file used for a scene is now by specification, not by convention. Yay.

--- test/com/deengames/gamebook/persistance/PersisterV1Test.hx
+++ test/com/deengames/gamebook/persistance/PersisterV1Test.hx
@@ -17,9 +17,9 @@ class PersisterV1Test
 	public function deserializeProjectReadsAllPropertiesCorrectly() : Void
 	{
 		var project:Project = new PersisterV1().deserializeProject(
-			"<project fileVersion=\"1\" name=\"Rayyan the Rabbit\" scenesFile=\"scenes.swf\">" +
-			"	<scene name=\"Scene 1\" background=\"beach\" backgroundAudio=\"seagulls.mp3\" text=\"Hi mom!\" />" +
-			"	<scene name=\"Scene 2\" background=\"scene2\" text=\"...\" />" +
+			"<project fileVersion='1' name='Rayyan the Rabbit' scenesFile='scenes.swf'>" +
+			"	<scene name='Scene 1' background='beach' narration='hi.mp3' backgroundAudio='seagulls.mp3' text='Hi mom!' />" +
+			"	<scene name='Scene 2' background='scene2' narration='ho.mp3' text='...' />" +
 			"</project>");
 			
 		Assert.areEqual("Rayyan the Rabbit", project.name);
@@ -28,45 +28,61 @@ class PersisterV1Test
 		var s1:Scene = project.getScene(0);
 		Assert.areEqual("Scene 1", s1.name);
 		Assert.areEqual("beach", s1.background);
+		Assert.areEqual("hi.mp3", s1.narration);
 		Assert.areEqual("Hi mom!", s1.text);
 		Assert.areEqual("seagulls.mp3", s1.backgroundAudio);
 	}
 	
-	@Test
-	public function deserializeThrowsAnExceptionIfBackgroundIsMissing() : Void
-	{
-		try {
-			var project:Project = new PersisterV1().deserializeProject(
-				"<project fileVersion=\"1\" name=\"Rayyan the Rabbit\" scenesFile=\"hi.swf\" >" +
-				"	<scene name=\"Scene 1\" background=\"\" text=\"Hi mom!\" />" +
-				"</project>");
-			Assert.fail("Exception excepted; didn't appear.");
-		} catch (error:Dynamic) {
-			Assert.isTrue(true);
-		}
-	}
-	
-	@Test
-	public function deserializeThrowsAnExceptionIfTextIsMissing() : Void
-	{
-		try {
-			var project:Project = new PersisterV1().deserializeProject(
-				"<project fileVersion=\"1\" name=\"Rayyan the Rabbit\" scenesFile=\"hi.swf\">" +
-				"	<scene name=\"Scene 1\" background=\"hi\" text=\"\" />" +
-				"</project>");
-			Assert.fail("Exception excepted; didn't appear.");
-		} catch (error:Dynamic) {
-			Assert.isTrue(true);
-		}
-	}
 	
 	@Test
 	public function deserializeThrowsAnExceptionIfProjectScenesFileIsMissing() : Void
 	{
 		try {
 			var project:Project = new PersisterV1().deserializeProject(
-				"<project fileVersion=\"1\" name=\"Rayyan the Rabbit\">" +
-				"	<scene name=\"Scene 1\" background=\"hi\" text=\"\" />" +
+				"<project fileVersion='1' name='Rayyan the Rabbit'>" +
+				"	<scene name='Scene 1' background='hi' narration='ho' text='hee' />" +
+				"</project>");
+			Assert.fail("Exception excepted; didn't appear.");
+		} catch (error:Dynamic) {
+			Assert.isTrue(true);
+		}
+	}
+	
+	@Test
+	public function deserializeThrowsAnExceptionIfSceneBackgroundIsMissing() : Void
+	{
+		try {
+			var project:Project = new PersisterV1().deserializeProject(
+				"<project fileVersion='1' name='Rayyan the Rabbit' scenesFile='hi.swf' >" +
+				"	<scene name='Scene 1' background='' narration='hi' text='Hi mom!' />" +
+				"</project>");
+			Assert.fail("Exception excepted; didn't appear.");
+		} catch (error:Dynamic) {
+			Assert.isTrue(true);
+		}
+	}
+	
+	@Test
+	public function deserializeThrowsAnExceptionIfSceneTextIsMissing() : Void
+	{
+		try {
+			var project:Project = new PersisterV1().deserializeProject(
+				"<project fileVersion='1' name='Rayyan the Rabbit' scenesFile='hi.swf'>" +
+				"	<scene name='Scene 1' background='hi' narration='narrate1' text='' />" +
+				"</project>");
+			Assert.fail("Exception excepted; didn't appear.");
+		} catch (error:Dynamic) {
+			Assert.isTrue(true);
+		}
+	}
+	
+	@Test
+	public function deserializeThrowsAnExceptionIfSceneNarrationIsMissing() : Void
+	{
+		try {
+			var project:Project = new PersisterV1().deserializeProject(
+				"<project fileVersion='1' name='Rayyan the Rabbit' scenesFile='hi.swf'>" +
+				"	<scene name='Scene 1' background='hi' narration='' text='ahem' />" +
 				"</project>");
 			Assert.fail("Exception excepted; didn't appear.");
 		} catch (error:Dynamic) {
